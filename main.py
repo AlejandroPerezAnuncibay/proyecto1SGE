318eb02a5e20d86bdc94285c206d8ba09d542a1c
BECARIO INFORMATICA <binformatica@DOMIALPHA.DIOCESANAS.ORG>
2021-12-10 09:42:20 +0100

    primer commit

---
+++ main.py
@@ -0,0 +1,70 @@
+from termcolor import colored as color
+
+
+
+def creacionDatos():
+    global usuarios
+    '''
+    La logica que he seguido es que si esta lista fuera una base de datos, antes de insertar los datos
+    siempre los transformaria a mayusculas para que a la hora de comparar los datos no de problema
+    '''
+    usuarios = ["ALEJANDRO", "EKAITZ", "JUAN", "MARIO"]
+    inicio()
+
+def inicio():
+    print(color('Introduce el nombre de usuario que desees:', 'green'))
+    nombre = input()
+    if nombre.upper() in usuarios:
+        print(color('El nombre de usuario ya existe, pruebe otra vez..', 'red'))
+        inicio()
+    else:
+        usuarios.append(nombre.upper())
+        print(color('Usuario añadido correctamente', 'green'))
+        generadorContrasena()
+
+
+def generadorContrasena():
+    print(color('A continuación generaremos una contraseña..', 'green'))
+    longitud = pedirLongitud()
+    minusculas = pedirMinusculas()
+    mayusculas = pedirMayusculas()
+
+
+def pedirMayusculas():
+    print(color('¿Quieres que tu contraseña contenga minisuculas? (si o no)', 'green'))
+    respuesta = input()
+    if respuesta.lower() == "si":
+        return True
+    elif respuesta.lower() == "no":
+        return False
+    else:
+        print(color('Por favor introduce si o no...', 'red'))
+        pedirMinusculas()
+
+
+
+def pedirMinusculas():
+    print(color('¿Quieres que tu contraseña contenga minisuculas? (si o no)', 'green'))
+    respuesta = input()
+    if respuesta.lower() == "si":
+        return True
+    elif respuesta.lower() == "no":
+        return False
+    else:
+        print(color('Por favor introduce si o no...', 'red'))
+        pedirMinusculas()
+
+def pedirLongitud():
+    print(color('Introduce la longitud de la contraseña(tiene que ser un numero postivo y entero)', 'green'))
+    longitud = input()
+    if longitud.isnumeric:
+        if int(longitud) > 0:
+            return longitud
+        else:
+            print(color('El número introducido es incorrecto, por favor compruebe los requisitos...', 'red'))
+            pedirLongitud()
+
+
+
+if __name__ == '__main__':
+    creacionDatos()
