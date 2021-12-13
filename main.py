87c2fc4dd9ba90bf9b1a343bbb2646fae43bc65a
AlejandroPerezAnuncibay <alejandro.perez@ikasle.egibide.org>
2021-12-13 17:27:49 +0100

    Errores varios y codigo documentado

--- main.py
+++ main.py
@@ -1,58 +1,67 @@
+# Imports de assets que utilizo en la aplicación
 from termcolor import colored as color
 import random
 import re
 import smtplib
+import sys
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import sys
 
 
-#Funcion que se encarga de generar los datos y comenzar la app
+# Funcion que se encarga de generar los datos y comenzar la app
 def creacionDatos():
-    global usuarios
-    global departamentos
+    global usuarios  # Array de usuarios que voy a utilizar como base de datos
+    global departamentos  # Array de departamentos que voy a utilizar como base de datos
     '''
     La logica que he seguido es que si esta lista fuera una base de datos, antes de insertar los datos
     siempre los transformaria a mayusculas para que a la hora de comparar los datos no de problema
     '''
-    usuarios = [["ALEJANDRO", "A23441ASDF", "MARKETING", "alejandroperez@gmail.com"], ["EKAITZ", "ASEF432Q562ASDF", "VENTAS", "ekaitz@gmail.com"], ["JUAN", "ase4fm349", "COMPRAS", "juanfrancisco@gmail.com"]
+    usuarios = [["ALEJANDRO", "A23441ASDF", "MARKETING", "alejandroperez@gmail.com"],
+                ["EKAITZ", "ASEF432Q562ASDF", "VENTAS", "ekaitz@gmail.com"],
+                ["JUAN", "ase4fm349", "COMPRAS", "juanfrancisco@gmail.com"]
         , ["MARIO", "AS34Rasrg", "ALMACEN", "mariozaton@gmail.com"]]
     departamentos = ["VENTAS", "COMPRAS", "MARKETING", "ALMACEN", "ADMINISTRACION"]
     inicio()
 
-#Funcion principal que se encarga de llamar al resto de funciones y de recoger los datos
+
+# Funcion principal que se encarga de llamar al resto de funciones y de recoger los datos
 def inicio():
     print(color('Introduce el nombre de usuario que desees:', 'green'))
     nombre = input()
-    if nombre.upper() in usuarios:
-        print(color('El nombre de usuario ya existe, pruebe otra vez..', 'red'))
-        inicio()
-    elif str.isspace(nombre) or len(nombre)==0:
+
+    for x in usuarios:
+        if x[0] == nombre.upper():
+            print(color('El nombre de usuario ya existe, pruebe otra vez..', 'red'))
+            inicio()
+
+    if str.isspace(nombre) or len(nombre) == 0:
         print(color('El nombre no puede estar vacio, pruebe otra vez...', 'red'))
         inicio()
     else:
         contrasena = generadorContrasena()
     departamento = preguntarDpto()
     email = preguntarEmail()
-    user = [nombre, contrasena, departamento, email]
-    print(color('Usuario añadido correctamente', 'green'))
+    user = [nombre.upper(), contrasena, departamento, email]
+    print(color('Usuario añadido correctamente', 'blue'))
 
     usuarios.append(user)
     print(color('Sus datos son:\n'
-                'Nombre: '+ nombre+'\n'
-                'Contraseña: '+str(contrasena)+'\n'
-                'Departamento: '+str(departamento)+'\n'
-                'Email: '+ str(email), 'yellow'))
+                'Nombre: ' + nombre + '\n'
+                'Contraseña: ' + str(contrasena) + '\n'
+                'Departamento: ' + str(departamento) + '\n'
+                'Email: ' + str(email), 'blue'))
 
-    print(color('Ahora mismo hay '+str(len(usuarios))+' usuarios', 'white'))
+    print(color('Ahora mismo hay ' + str(len(usuarios)) + ' usuarios', 'blue'))
     mandarCorreo(user)
 
-#Funcion que se encarga de mandar el correo electronico con la informacion generada
+
+# Funcion que se encarga de mandar el correo electronico con la informacion generada
 def mandarCorreo(user):
+    # Configuracion del servidor de correo electronico. En este caso he utilizado el de gmail
     server = smtplib.SMTP('smtp.gmail.com: 587')
     msg = MIMEMultipart()
-
-    message = "Usuario: "+str(user[0])+"\nContraseña: "+str(user[1])+"\nDepartamento: "+str(user[2])
+    # Cuerpo del correo electronico
+    message = "Usuario: " + str(user[0]) + "\nContraseña: " + str(user[1]) + "\nDepartamento: " + str(user[2])
 
     password = "Jm12345Jm"
     msg['From'] = "pruebapythonalejandro@gmail.com"
@@ -64,11 +73,13 @@ def mandarCorreo(user):
     server.login(msg['From'], password)
     server.sendmail(msg['From'], msg['To'], msg.as_string())
     mensaje = "Se ha enviado correctamente el correo a %s" % (msg['To'])
-    print(color(str(mensaje), 'blue'))
+    print(color(str(mensaje), 'red'))
     server.quit()
     sys.exit()
 
-#Funcion que se encarga de recoger el correo electronico validado
+
+# Funcion que se encarga de recoger el correo electronico validado
+
 def preguntarEmail():
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     print(color('Introduzca su correo electronico', 'green'))
@@ -80,24 +91,29 @@ def preguntarEmail():
         print(color('Correo incorrecto, por favor compruebalo', 'red'))
         return preguntarEmail()
 
-
+#Funcion que se encarga de pedir el departamento al que va a pertenecer el usuario
 def preguntarDpto():
     print(color('Escoge tu nuevo departamento:', 'green'))
 
     for x in departamentos:
-        print(color(str(departamentos.index(x))+": "+x,'yellow'))
+        print(color(str(departamentos.index(x)) + ": " + x, 'blue'))
 
     departamento = input()
-    if departamento.isnumeric() and len(departamento) != 0 and str.isspace(departamento) == False:
+    if departamento.isnumeric() and len(departamento) != 0 and str.isspace(departamento) is False:
         if 4 >= int(departamento) >= 0:
             return departamentos[int(departamento)]
 
     print(color("Por favor introduce un departamento valido", 'red'))
     return preguntarDpto()
 
+#Funcion que se encarga de generar la contraseña
 
-
-
+'''
+En esta función lo que primero recojo es la longitud deseada por el usuario. No puede ser 0 ni vacio ni espacios
+en blanco. En caso de que sea menor que 8 le aconsejare que ponga como minimo 8 caracteres. Tras ello le preguntaremos 
+que tipo de caracteres va a querer que tenga su contraseña. Como minimo tendrá que aceptar uno. Depende de cuantos
+tipos haya añadido, sumará 1 punto a seguridad, yendo de menos seguro a más.
+'''
 
 def generadorContrasena():
     print(color('A continuación generaremos una contraseña..', 'green'))
@@ -131,19 +147,24 @@ def generadorContrasena():
     else:
         print(color('Su contraseña es de seguridad alta', 'blue'))
 
-
     contrasena = crearContrasena(longitud, minusculas, mayusculas, simbolos, numeros)
     return contrasena
 
-
-
+#Funcion que se encarga de generar la contraseña aleatoriamente
+'''
+Tengo un array con cada uno de los caracteres que se pueda utilizar. En caso de que el usuario diga que quiere usar
+esos caracteres, se añadirá el array con esos caracteres a un array que los contendra todos, despues jugando
+con los tamaños de los arrays. Primero se genera un número random con los arrays introducidos y despues uno con la
+longitud del mismo, asi se escogera un array random y un caracter random.
+'''
 def crearContrasena(longitud, minusculas, mayusculas, simbolos, numeros):
     contrasena = ""
     listaMinusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
                        'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    listaMayusculas = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    listaNumeros = ['1','2','3','4','5','6','7','8','9','0']
-    listaSimbolos = ['!','@','#','*','?']
+    listaMayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                       'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    listaNumeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    listaSimbolos = ['!', '@', '#', '*', '?']
     listadelistas = []
     if minusculas:
         listadelistas.append(listaMinusculas)
@@ -159,15 +180,15 @@ def crearContrasena(longitud, minusculas, mayusculas, simbolos, numeros):
         return generadorContrasena()
     else:
         while x < int(longitud):
-            numeroRandom = random.randint(0, len(listadelistas)-1)
-            numerorandom2 = random.randint(0,len(listadelistas[numeroRandom])-1)
+            numeroRandom = random.randint(0, len(listadelistas) - 1)
+            numerorandom2 = random.randint(0, len(listadelistas[numeroRandom]) - 1)
             contrasena = contrasena + listadelistas[numeroRandom][numerorandom2]
-            x = x +1
+            x = x + 1
 
-    print(color("Su nueva contraseña es = ", 'blue')+color(contrasena, 'red'))
+    print(color("Su nueva contraseña es = ", 'blue') + color(contrasena, 'red'))
     return contrasena
 
-
+#Funcion encargada de comprobar que como minimo tenga 8 caracteres y en su defecto preguntar para cambiarlo.
 def contrasenaLongitud():
     print(color('Su contraseña tiene menos de 8 caracteres, se recomienda por su seguridad que '
                 'tenga más de 8 caracteres. ¿Quiere establecerla de 8 caracteres?', 'red'))
@@ -181,11 +202,9 @@ def contrasenaLongitud():
         print(color('Por favor escoja una opción existente...', 'red'))
         return contrasenaLongitud()
 
-
-
-
+#Funcion reutilizada para pedir los distintos tipos de caracteres que contendrá la contraseña.
 def pedirdato(dato):
-    print(color('¿Quieres que tu contraseña contenga '+dato+'? (si o no)', 'green'))
+    print(color('¿Quieres que tu contraseña contenga ' + dato + '? (si o no)', 'green'))
     respuesta = input()
     if respuesta.lower() == "si":
         return True
@@ -195,7 +214,7 @@ def pedirdato(dato):
         print(color('Por favor introduce si o no...', 'red'))
         return pedirdato(dato)
 
-
+#Funcion encargada de comprobar la longitud introducida por el usuario para la contraseña
 def pedirLongitud():
     print(color('Introduce la longitud de la contraseña(tiene que ser un numero postivo y entero)', 'green'))
     longitud = input()
